e11026147a36b54690859f1bd4090090b5a76126
Krasimira Arizanova <70700274+KrasyAri@users.noreply.github.com>
2021-02-17 16:30:42 +0200

    files

---
+++ EntityFrameworkCore/ADO.Net/2. Villain Names.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _2._Villain_Names
+{
+    class Program
+    {
+        const string sqlConnectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Database=MinionsDB";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(sqlConnectionString))
+            {
+                connection.Open();
+
+                string query = @"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                        FROM Villains AS v 
+                                       JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                       GROUP BY v.Id, v.Name 
+                                             HAVING COUNT(mv.VillainId) > 3 
+                                            ORDER BY COUNT(mv.VillainId)";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var name = reader[0];
+                            var count = reader[1];
+
+                            Console.WriteLine($"{name} - {count}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
